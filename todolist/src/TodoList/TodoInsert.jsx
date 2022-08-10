3b459caccc2babcb99e40046b9180ad7f2687625
89882 <pgh123166@naver.com>
2022-08-10 14:34:13 +0900

    feat: 전체 목록 조회

--- todolist/src/TodoList/TodoInsert.jsx
+++ todolist/src/TodoList/TodoInsert.jsx
@@ -1,17 +1,18 @@
 import React ,{useState, useCallback} from "react";
 import axios from 'axios';
 
-function TodoInsert(){
+function TodoInsert(props){
+    const value= props.value;
+    const setValue=props.setValue;
     const url="http://localhost:5000/todo";
-    const [value , setValue] = useState('');
-    const onChange = useCallback(e=>{
-        setValue(e.target.value);
-    },[]);
     //변화가 일어났을 때 post로 정보를 추가한다
-
-const onSubmit = useCallback(e=>{
-    setValue('');
+const onChange = useCallback((e)=>{
+setValue(e.target.value);
+});
+//입력하면 그 값을 value 에 넣어줌
+const onSubmit = useCallback( (e)=>{
     e.preventDefault();
+    setValue('');
     axios.post(url,{"content":value})
     .then(function(response){
         console.log("성공");
@@ -21,10 +22,11 @@ const onSubmit = useCallback(e=>{
         console.log("실패");
         console.log(error);
     })
-},[value]);
+});
+//제출 버튼을 누르면 그 value값을 post해서 전송
     return(
             <form className='insert'>
-                <input onChange={onChange} value={value}size="100" type="text"className='insertInput' placeholder="what needs to be done?"/>
+                <input onChange={onChange} value={value} size="100" type="text"className='insertInput' placeholder="what needs to be done?"/>
                 <button onClick={onSubmit}className='insertButton'> +</button>
             </form>
     );
--- todolist/src/TodoList/TodoTemplate.jsx
+++ todolist/src/TodoList/TodoTemplate.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React , {useState} from "react";
 import TodoInsert from './TodoInsert';
 import TodoList from'./TodoList';
 import TodoButtom from'./TodoButtom';
+
 function TodoTemplate(){
+    const [value , setValue] = useState('');
+    //value가 바뀔 때 마다 다시 렌더링 하게 됨
 return(
     <div>
         <div className='todoListFont'>Todo List</div>
         <div className='template'>
-            <TodoInsert></TodoInsert>
-            <TodoList></TodoList>
+            <TodoInsert  value={value} setValue={setValue}></TodoInsert>
+            <TodoList value={value} ></TodoList>
             <TodoButtom></TodoButtom>
         </div>
     </div>        
