a22caf2357848a0d5fd41dbbb63824a77d36f87f
89882 <pgh123166@naver.com>
2022-08-11 11:31:03 +0900

    fix:보여주기 에러 수정

--- todolist/src/TodoList/TodoList.jsx
+++ todolist/src/TodoList/TodoList.jsx
@@ -1,27 +1,26 @@
-import React , {useState , useEffect}from "react";
+import React , { useState, useEffect}from "react";
 import TodoItem from "./TodoItem";
 import axios from"axios";
 
 function TodoList(props){
-    const value=props.value;
-    console.log("value는"+value)
+    const change=props.change;
+    const setChange=props.setChange;
     const [items , setItems] =useState([]);
     const url="http://localhost:5000/todo";
     useEffect(()=>{
         axios.get(url).then(function(response){
         setItems(response.data);
         console.log("보여주기 성공");
-        console.log(value);
     })
     .catch(function(error){
         console.log("보여주기 실패");
         console.log(error);
     })
-    },[value]);
+    },[change]);
     //업데이트로 인한 렌더링이 될때마다 보여지는거 
     //수정, 삭제, value값 추가 모두 항목 조회랑 연관있는 업데이트이기에 빈 배열을 없애면 안에 setItem이 있어서 item 변경으로 무한 렌더링이된다
     return(
-        <div className="todoList"><ul>{items.map((item)=><TodoItem key={item.id} item={item}></TodoItem>)}</ul></div>
+        <div className="todoList"><ul>{items.map((item)=><TodoItem key={item.id} item={item} change={change} setChange={setChange}></TodoItem>)}</ul></div>
     );
 }
 
--- todolist/src/TodoList/TodoTemplate.jsx
+++ todolist/src/TodoList/TodoTemplate.jsx
@@ -6,13 +6,13 @@ import TodoButtom from'./TodoButtom';
 function TodoTemplate(){
     const [value , setValue] = useState('');
     //value가 바뀔 때 마다 다시 렌더링 하게 됨
-    console.log("이거"+value);
+    const [change, setChange] = useState(0);
 return(
     <div>
         <div className='todoListFont'>Todo List</div>
         <div className='template'>
-            <TodoInsert  value={value} setValue={setValue}></TodoInsert>
-            <TodoList value={value} ></TodoList>
+            <TodoInsert change={change} setChange={setChange} value={value} setValue={setValue}></TodoInsert>
+            <TodoList change={change} setChange={setChange} value={value} ></TodoList>
             <TodoButtom></TodoButtom>
         </div>
     </div>        
